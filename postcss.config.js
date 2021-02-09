ab5b3bc6a21ba05e9cdb79e8d4da0d14cffa2b2c
Fernando Ricaurte <fernando.ricaurte@hotmail.com>
2021-02-08 23:21:19 -0500

    Configure libraries and database

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
+    require('autoprefixer'),
+    require('tailwindcss')('./app/javascript/src/tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
